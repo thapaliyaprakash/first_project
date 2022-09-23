023e7de60f896a1b791be3bd6c1c35d434d848be
Prakash Thapaliya <holmsnick34@gmail.com>
2022-09-23 19:12:51 +0545

    useful website

--- Learn React/first/src/components/City.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const City = (props) => {
-    return (
-    <p>Hello, I am in {props.city}. It is {props.feature}.</p>
-    )
-}
-export default City
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
-import './App.css';
-import Todo from './components/Todo'
-
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+import Button from './Button'
+const App=()=> {
+  return <div>
+    <h1>Useful Website</h1>
+    <h3>Which website do you want to open?</h3>
+    <Button name="Google" link="https://google.com"/>
+    <Button name="Facebook"link="https://facebook.com"/>
+    <Button name="YouTube"link="https://youtube.com"/>
+    <br></br>
+    <Button name="Github"link="https://github.com/thapaliyaprakash"/>
+  </div>
 }
-
 export default App;
